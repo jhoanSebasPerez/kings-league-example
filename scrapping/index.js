4ae8e0b98789c1b1fc14971703f1d2b158685c95
jhoanperez <jhoansebastianperez73@gmail.com>
2023-01-05 09:51:43 -0500

    Add package json with dependencies

---
+++ scrapping/index.js
@@ -0,0 +1,56 @@
+import * as cheerio from 'cheerio'
+import { writeFile } from 'node:fs/promises'
+import path from 'node:path'
+
+const URLS = {
+  leaderboard: 'https://kingsleague.pro/estadisticas/clasificacion/'
+}
+
+async function scrape (url) {
+  const res = await fetch(url)
+  const html = await res.text()
+
+  return cheerio.load(html)
+}
+
+async function getLeaderboard () {
+  const $ = await scrape(URLS.leaderboard)
+  const $row = $('table tbody tr')
+
+  const LEADERBOARD_SELECTORS = {
+    team: { selector: '.fs-table-text_3', typeOf: 'string' },
+    wins: { selector: '.fs-table-text_4', typeOf: 'number' },
+    loses: { selector: '.fs-table-text_5', typeOf: 'number' },
+    goalsScored: { selector: '.fs-table-text_6', typeOf: 'number' },
+    goalsConceded: { selector: '.fs-table-text_7', typeOf: 'number' },
+    cardsYellow: { selector: '.fs-table-text_8', typeOf: 'number' },
+    cardsRed: { selector: '.fs-table-text_9', typeOf: 'number' }
+  }
+
+  const cleanText = text => text
+    .replace(/\t|\n|\s:/g, '')
+    .replace(/.*:/g, '')
+    .trim()
+
+  const leaderboard = []
+  $row.each((index, el) => {
+    const $el = $(el)
+
+    const leaderBoardSelectorsEntries = Object.entries(LEADERBOARD_SELECTORS)
+    const leaderBoardEntries = leaderBoardSelectorsEntries.map(([key, { selector, typeOf }]) => {
+      const rawValue = $el.find(selector).text()
+      const cleanedValue = cleanText(rawValue)
+      const value = typeOf === 'number' ? Number(cleanedValue) : cleanedValue
+      return [key, value]
+    })
+    leaderboard.push(Object.fromEntries(leaderBoardEntries))
+  })
+
+  return leaderboard
+}
+
+const leaderboard = await getLeaderboard(URLS.leaderboard)
+
+const pathFile = path.join(process.cwd(), './db/leaderboard.json')
+
+await writeFile(pathFile, JSON.stringify(leaderboard, null, 2), 'utf-8')
