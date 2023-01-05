9f66820588ab5ca779cb6f0808abecaf24b42fdb
jhoanperez <jhoansebastianperez73@gmail.com>
2023-01-05 15:55:47 -0500

    basic endpoints, leaderboard and entry point

---
+++ api/index.js
@@ -0,0 +1,14 @@
+import { Hono } from 'hono'
+import leaderboard from '../db/leaderboard.json'
+
+const app = new Hono()
+
+app.get('/', (ctx) => {
+  return ctx.json([{ endpoint: '/leaderboard', description: 'return all teams from leaderboard' }])
+})
+
+app.get('/leaderboard', (ctx) => {
+  return ctx.json(leaderboard)
+})
+
+export default app
